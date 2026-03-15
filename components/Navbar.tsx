08a9e821ebb855819f8091a7bc2ff9a6520a7d5e
A7med-07 <ahmed.0.7sabry.0.5@gmail.com>
2026-03-15 22:41:29 +0200

    nav commit

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -45,7 +45,7 @@ export default function Navbar() {
           {/* Logo */}
           <div className="flex-shrink-0">
             <span className="text-2xl font-bold gradient-text cursor-pointer" onClick={() => scrollToSection('hero')}>
-              AS
+             <a className="text-xl font-bold transition-all"  href="#hero">&lt;Ahmed /&gt;</a>
             </span>
           </div>
 
